f1d0e28372f9760629bedb0aebea568867fe78d0
MarioZavala <mrzavalat@alumno.uspg.edu.gt>
2021-08-19 12:12:46 -0600

    primer commit

---
+++ src/main/java/edu/uspg/FabricaEjercicioApplication.java
@@ -0,0 +1,13 @@
+package edu.uspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaEjercicioApplication.class, args);
+	}
+
+}
